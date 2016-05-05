0e63c4c5e20e23338fa5d54eb0cd70f680fdc72d
Georg Sauthoff <mail@georg.so>
2016-05-05 10:26:58 +0200

    Also add -k option to C++ variant

--- chronic.cc
+++ chronic.cc
@@ -1,5 +1,5 @@
 static const char help_str[] =
-"Call: %s COMMAND [ARG..]\n"
+"Call: %s [OPT..] COMMAND [ARG..]\n"
 "\n"
 "Silence stdout/stderr of COMMAND unless its return code is unequal to 0.\n"
 "\n"
@@ -8,7 +8,13 @@ static const char help_str[] =
 " - wrap commands that are called from a job scheduler like cron\n"
 " - increase the signal-to-noise-ratio in the terminal\n"
 "\n"
-"The program doesn't have any options besides -h/--help.\n"
+"Options:\n"
+"\n"
+"-h,--help   this screen\n"
+"-k,-K       enable/disable suicide on parent exit (default: disabled)\n"
+"            On Linux, a parent death signal is installed in the child\n"
+"            that execs COMMAND, otherwise the TERM signal handler kills\n"
+"            the child.\n"
 "\n"
 "It honors the TMPDIR environment and defaults to /tmp in case\n"
 "it isn't set.\n"
@@ -18,9 +24,25 @@ static const char help_str[] =
 "on the differences.\n"
 "\n"
 "\n"
-"2016, Georg Sauthoff <mail@georg.so>\n"
-"GPLv3+\n"
+"2016, Georg Sauthoff <mail@georg.so>, GPLv3+\n"
+"cf. https://github.com/gsauthof/utility\n"
 "\n";
+
+#ifndef USE_TMPFILE
+  #if defined(__linux__)
+    #define USE_TMPFILE 1
+  #else
+    #define USE_TMPFILE 0
+  #endif
+#endif
+
+#ifndef USE_PRCTL
+  #if defined(__linux__)
+    #define USE_PRCTL 1
+  #else
+    #define USE_PRCTL 0
+  #endif
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -33,6 +55,10 @@ static const char help_str[] =
 #include <fcntl.h>
 #include <unistd.h>
 
+#if defined(__linux__)
+  #include <sys/prctl.h>
+#endif
+
 #include <ixxx/ixxx.hh>
 using namespace ixxx;
 
@@ -41,9 +67,40 @@ static void help(FILE *f, const char *argv0)
   fprintf(f, help_str      , argv0);
 }
 
+struct Arguments {
+  const char *tmpdir { "/tmp" };
+  bool suicide { false};
+};
+
+static char **parse_arguments(int argc, char **argv, Arguments &a)
+{
+  const char *tmpdir = getenv("TMPDIR");
+  if (tmpdir)
+    a.tmpdir = tmpdir;
+  if (argc > 1 && !strcmp(argv[1], "--help")) {
+    help(stdout, argv[0]);
+    exit(0);
+  }
+  char c = 0;
+  while ((c = getopt(argc, argv, "+kKh")) != -1) {
+    switch (c) {
+      case 'k': a.suicide = true ; break;
+      case 'K': a.suicide = false; break;
+      case 'h': help(stdout, argv[0]); exit(0); break;
+      case '?': help(stderr, argv[0]); exit(1); break;
+    }
+  }
+  if (optind == argc) {
+    help(stderr, argv[0]);
+    exit(1);
+  }
+  return argv + optind;
+}
+
+
 static int create_unlinked_temp_file(const char *tmpdir)
 {
-#if defined(__linux__)
+#if USE_TMPFILE
   int fd = posix::open(tmpdir, O_RDWR | O_TMPFILE | O_EXCL, 0600);
   return fd;
 #else
@@ -116,21 +173,46 @@ static void dump(int fd, int d)
   }
 }
 
-static void supervise_child(int fd_o, int fd_e, pid_t pid)
+#if !USE_PRCTL
+
+static pid_t child_pid_ = 0;
+
+static void kill_child(int sig)
+{
+  if (child_pid_)
+    kill(child_pid_, SIGTERM);
+  exit(128 + SIGTERM);
+}
+
+#endif
+
+static void supervise_child(int fd_o, int fd_e, pid_t pid, const Arguments &a)
 {
   // we ignore QUIT/INT because when issued via Ctrl+\/Ctrl+C in the terminal,
   // UNIX sends them both to the parent and the child
   // (cf. http://unix.stackexchange.com/questions/176235/fork-and-how-signals-are-delivered-to-processes)
   // ignoring them in the parent thus makes sure that any collected output is printed
   // after the child terminates because of those signals (the default action)
-  struct sigaction ignore_action;
+  struct sigaction ignore_action = {};
   ignore_action.sa_handler = SIG_IGN;
   struct sigaction old_int_action;
   posix::sigaction(SIGINT, &ignore_action, &old_int_action);
   struct sigaction old_quit_action;
   posix::sigaction(SIGQUIT, &ignore_action, &old_quit_action);
+#if !USE_PRCTL
+  child_pid_ = pid;
+  struct sigaction term_action = {};
+  term_action.sa_handler = kill_child;
+  struct sigaction old_term_action;
+  if (a.suicide)
+    posix::sigaction(SIGTERM, &term_action, &old_term_action);
+#endif
   siginfo_t siginfo;
   posix:: waitid(P_PID, pid, &siginfo, WEXITED);
+#if !USE_PRCTL
+  if (a.suicide)
+    posix::sigaction(SIGTERM, &old_term_action, 0);
+#endif
   posix::sigaction(SIGINT, &old_int_action, 0);
   posix::sigaction(SIGQUIT, &old_quit_action, 0);
   int code = siginfo.si_code == CLD_EXITED
@@ -146,7 +228,7 @@ static void exec_child(int fd_o, int fd_e, char **argv)
 {
   posix::dup2(fd_o, 1);
   posix::dup2(fd_e, 2);
-  int r = execvp(argv[1], argv+1);
+  int r = execvp(*argv, argv);
   if (r == -1) {
     perror("executing command");
     // cf. http://tldp.org/LDP/abs/html/exitcodes.html
@@ -174,25 +256,27 @@ static void exec_child(int fd_o, int fd_e, char **argv)
    */
 int main(int argc, char **argv)
 {
-  if (argc == 1) {
-    help(stderr, argv[0]);
-    return 1;
-  }
-  if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
-    help(stdout, argv[0]);
-    return 0;
-  }
   try {
-    const char *tmpdir = getenv("TMPDIR");
-    if (!tmpdir)
-      tmpdir = "/tmp";
-    int fd_o = create_unlinked_temp_file(tmpdir);
-    int fd_e = create_unlinked_temp_file(tmpdir);
+    Arguments a;
+    char **childs_argv = parse_arguments(argc, argv, a);
+    int fd_o = create_unlinked_temp_file(a.tmpdir);
+    int fd_e = create_unlinked_temp_file(a.tmpdir);
+#if USE_PRCTL
+    pid_t ppid_before_fork = getpid();
+#endif
     pid_t pid = posix::fork();
-    if (pid)
-      supervise_child(fd_o, fd_e, pid);
-    else
-      exec_child(fd_o, fd_e, argv);
+    if (pid) {
+      supervise_child(fd_o, fd_e, pid, a);
+    } else {
+#if USE_PRCTL
+    if (a.suicide) {
+      linux::prctl(PR_SET_PDEATHSIG, SIGTERM);
+      if (getppid() != ppid_before_fork)
+        exit(1);
+    }
+#endif
+      exec_child(fd_o, fd_e, childs_argv);
+    }
   } catch (const ixxx::errno_error &e) {
     fprintf(stderr, "%s\n", e.what());
     return 1;
