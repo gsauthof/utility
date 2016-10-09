b4a4d3c199eefd5613eaeed416adc5306e0c3c06
Georg Sauthoff <mail@georg.so>
2016-10-09 11:25:18 +0200

    fix script for CentOS 7 perf output

--- perfstat.sh
+++ perfstat.sh
@@ -41,15 +41,24 @@ tfile=$(mktemp)
 perf stat -x, -o "$tfile" "$@"
 
 awk -F, '
-  $3 == "task-clock:u" { a["usec"]=$4; next }
-  $3 == "context-switches:u" { a["cswitch"]=$1; next }
-  $3 == "cpu-migrations:u" { a["cpu_migr"]=$1; next }
-  $3 == "page-faults:u" { a["page_fault"]=$1; next }
-  $3 == "cycles:u" { a["cycles"]=$1; a["ghz"]=$6; next }
-  $3 == "instructions:u" { a["ins"]=$1; a["ins_cyc"]=$6}
-  $3 == "branches:u" { a["br"]=$1; }
-  $3 == "branch-misses:u" { a["br_mis"]=$1; a["br_mis_rate"]=$6 }
+  # on Fedora 23 the value have the :u suffix, on CentOS they do not
+  $3 ~ /^task-clock(:u)?$/       { a["usec"]=$4;                        next }
+  $3 ~ /^context-switches(:u)?$/ { a["cswitch"]=$1;                     next }
+  $3 ~ /^cpu-migrations(:u)?$/   { a["cpu_migr"]=$1;                    next }
+  $3 ~ /^page-faults(:u)?$/      { a["page_fault"]=$1;                  next }
+  $3 ~ /^cycles(:u)?$/           { a["cycles"]=$1;     a["ghz"]=$6;     next }
+  $3 ~ /^instructions(:u)?$/     { a["ins"]=$1;        a["ins_cyc"]=$6; next }
+  $3 ~ /^branches(:u)?$/         { a["br"]=$1;                          next }
+  $3 ~ /^branch-misses(:u)?$/    { a["br_mis"]=$1;     a["br_mis_rate"]=$6;  }
   END {
-    OFS=","
+    OFS=",";
+    # CentOS 7 perf does not print those with -x ...
+    if (!a["ghz"])
+      a["ghz"]=a["cycles"]/a["usec"];
+    if (!a["ins_cyc"])
+      a["ins_cyc"]=a["ins"]/a["cycles"];
+    if (!a["br_mis_rate"])
+      a["br_mis_rate"]=a["br_mis"]/a["br"]*100.0;
+
     print a["usec"],a["cswitch"],a["cpu_migr"],a["page_fault"],a["cycles"],a["ghz"],a["ins"],a["ins_cyc"],a["br"],a["br_mis"],a["br_mis_rate"]
   }' "$tfile" > "$ofile"
