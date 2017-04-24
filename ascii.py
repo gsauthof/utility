29fad9810f257e5439c29d2e473a275b10e9750e
Georg Sauthoff <mail@georg.so>
2017-04-24 17:11:34 +0200

    add unittests

--- ascii.py
+++ ascii.py
@@ -99,8 +99,85 @@ def explain(s):
   else:
     print('{:>3} = {} ({})'.format(s, *desc[s]))
 
-def main():
-  args = parse_args()
+if __name__ != '__main__':
+  import io
+  import unittest.mock as mock
+
+def test_default_table():
+  with mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+    main([])
+    assert fake_out.getvalue() == '''   00   01   10   11
+  NUL  SPC    @    `  00000
+  SOH    !    A    a  00001
+  STX    "    B    b  00010
+  ETX    #    C    c  00011
+  EOT    $    D    d  00100
+  ENQ    %    E    e  00101
+  ACK    &    F    f  00110
+  BEL    '    G    g  00111
+   BS    (    H    h  01000
+   HT    )    I    i  01001
+   LF    *    J    j  01010
+   VT    +    K    k  01011
+   FF    ,    L    l  01100
+   CR    -    M    m  01101
+   SO    .    N    n  01110
+   SI    /    O    o  01111
+  DLE    0    P    p  10000
+  DC1    1    Q    q  10001
+  DC2    2    R    r  10010
+  DC3    3    S    s  10011
+  DC4    4    T    t  10100
+  NAK    5    U    u  10101
+  SYN    6    V    v  10110
+  ETB    7    W    w  10111
+  CAN    8    X    x  11000
+   EM    9    Y    y  11001
+   SS    :    Z    z  11010
+  ESC    ;    [    {  11011
+   FS    <    \    |  11100
+   GS    =    ]    }  11101
+   RS    >    ^    ~  11110
+   US    ?    _  DEL  11111
+'''
+
+def test_8col_table():
+  with mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+    main(['-c', '8'])
+    assert fake_out.getvalue() == '''  000  001  010  011  100  101  110  111
+  NUL  DLE  SPC    0    @    P    `    p  0000
+  SOH  DC1    !    1    A    Q    a    q  0001
+  STX  DC2    "    2    B    R    b    r  0010
+  ETX  DC3    #    3    C    S    c    s  0011
+  EOT  DC4    $    4    D    T    d    t  0100
+  ENQ  NAK    %    5    E    U    e    u  0101
+  ACK  SYN    &    6    F    V    f    v  0110
+  BEL  ETB    '    7    G    W    g    w  0111
+   BS  CAN    (    8    H    X    h    x  1000
+   HT   EM    )    9    I    Y    i    y  1001
+   LF   SS    *    :    J    Z    j    z  1010
+   VT  ESC    +    ;    K    [    k    {  1011
+   FF   FS    ,    <    L    \    l    |  1100
+   CR   GS    -    =    M    ]    m    }  1101
+   SO   RS    .    >    N    ^    n    ~  1110
+   SI   US    /    ?    O    _    o  DEL  1111
+'''
+
+def test_explain():
+  with mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+    main(['-x', 'EOT'])
+    assert fake_out.getvalue() == 'EOT = End of Transmission (4)\n'
+
+def test_explain_all():
+  with mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+    main(['-x'])
+    o = fake_out.getvalue()
+    assert 'EOT = End of Transmission (4)\n' in o
+    assert ' RS = Record Separator (30)' in o
+    assert  o.splitlines().__len__() == 34
+
+def main(*a):
+  args = parse_args(*a)
   if args.explain:
     explain(args.explain)
   else:
