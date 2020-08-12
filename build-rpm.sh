dc6fd3c88fe81452bc0b836930103cda5333ea92
Georg Sauthoff <mail@gms.tf>
2020-08-12 23:49:44 +0200

    Fix switched copr directories, cleanup arguments

--- build-rpm.sh
+++ build-rpm.sh
@@ -7,4 +7,7 @@ if [ $# -gt 0 ]; then
     outdir=$1
 fi
 
-rpmbuild --without srpm --define "_sourcedir $PWD" --define "_specdir $PWD" --define "_rpmdir $outdir" --define "_srcrpmdir $outdir" --define "_builddir $PWD" -bb gms-utils.spec
+rpmbuild --without srpm --define "_sourcedir $PWD" \
+    --define "_specdir $PWD" --define "_builddir $PWD" \
+    --define "_rpmdir $outdir" --define "_srcrpmdir $outdir" \
+    -bb gms-utils.spec
