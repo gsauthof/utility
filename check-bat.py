21455774de2196278ad385d831a1b4dc3f36a53b
Georg Sauthoff <mail@gms.tf>
2021-07-21 21:08:44 +0200

    support Netio's network controlled power sockets

--- check-bat.py
+++ check-bat.py
@@ -3,19 +3,26 @@
 # Monitor battery charging on an Android device and alarm user when
 # the 80 % capacity mark is reached.
 #
+# Alternatively, it switches off a networked power socket if
+# the threshold is reached.
+#
 # Has to run inside termux environment, in a local session
-# that should has the wake-lock aquired.
+# that should have the wake-lock aquired.
 #
 # SPDX-FileCopyrightText: © 2020 Georg Sauthoff <mail@gms.tf>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import argparse
 import json
 import logging
 import subprocess
 import sys
 import time
 
-
+import urllib.request
+import base64
+import os
+import configparser
 
 log = logging.getLogger(__name__)
 
@@ -31,11 +38,30 @@ def setup_logging():
     logging.getLogger().handlers[0].setFormatter(
             Relative_Formatter(log_format, style='{'))
 
+def parse_args(*xs):
+    p = argparse.ArgumentParser()
+    p.add_argument('--netio', action='store_true',
+            help='remote control Netio power socket instead of alarm')
+    p.add_argument('--config', '-c', default=os.getenv('HOME', '.') + '/.config/check-bat.ini',
+            help='config file for netio settings (default: %(default)s)')
+    p.add_argument('--high', type=int, default=80,
+            help='alarm/power-off threshold (default: %(default)d)')
+    args = p.parse_args(*xs)
+    return args
 
 def status():
     o = subprocess.check_output(['termux-battery-status'], universal_newlines=True)
     d = json.loads(o)
     return d
+
+
+def power_switch(user, password, host, no, action):
+    # host == http://example.org or https://example.org
+    req = urllib.request.Request(f'{host}/netio.json',
+            f'{{"Outputs":[{{"ID":{no},"Action":{action}}}]}}'.encode())
+    req.add_header('Authorization',
+            'Basic ' + base64.b64encode(':'.join((user, password)).encode()).decode())
+    urllib.request.urlopen(req)
 
 
 def alarm():
@@ -56,6 +82,14 @@ def alarm():
 
 def main():
     setup_logging()
+    args = parse_args()
+    conf = None
+    if args.netio:
+        conf = configparser.ConfigParser()
+        conf.read(args.config)
+        power_switch(conf['netio']['user'], conf['netio']['password'],
+                conf['netio']['host'], conf['netio']['no'], 1)
+
     i = 0
     while True:
         d = status()
@@ -63,8 +97,12 @@ def main():
         t = d['temperature']
         log.info(f'{p:3d} % - {t:6.2f} C')
 
-        if p > 80:
-            return alarm()
+        if p > args.high:
+            if args.netio:
+                return power_switch(conf['netio']['user'], conf['netio']['password'],
+                        conf['netio']['host'], conf['netio']['no'], 0)
+            else:
+                return alarm()
 
         if d['plugged'] == 'UNPLUGGED':
             i += 1
